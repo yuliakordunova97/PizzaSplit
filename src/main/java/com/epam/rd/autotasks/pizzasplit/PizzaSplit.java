bfedfd3e10fe4f83ddb414ee335c9a0dc024adfb
yuliakordunova97 <kordunovayulia@gmail.com>
2022-03-16 10:12:36 +0200

    added files

---
+++ src/main/java/com/epam/rd/autotasks/pizzasplit/PizzaSplit.java
@@ -0,0 +1,12 @@
+package com.epam.rd.autotasks.pizzasplit;
+
+import java.util.Scanner;
+
+public class PizzaSplit {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
